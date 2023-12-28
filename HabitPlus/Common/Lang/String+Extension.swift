09831d1b7f70f378ae9f33e248c8f9a8267503ff
BorgesDev <borgesarthur.dev@gmail.com>
2023-12-28 06:49:22 -0300

    Email validation using regEx and loadingButtonView

---
+++ HabitPlus/Common/Lang/String+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  String+Extension.swift
+//  HabitPlus
+//
+//  Created by Arthur Borges on 27/12/23.
+//
+
+import Foundation
---
+++ HabitPlus/Common/View/LoadingButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingButtonView.swift
+//  HabitPlus
+//
+//  Created by Arthur Borges on 28/12/23.
+//
+
+import SwiftUI
+
+struct LoadingButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingButtonView()
+}
