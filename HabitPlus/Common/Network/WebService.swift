8d3507bce715dd46b43155fbf30c38ed93a500e9
BorgesDev <borgesarthur.dev@gmail.com>
2024-01-06 19:50:57 -0300

    Learning to use API

--- HabitPlus/Common/Network/WebService.swift
+++ HabitPlus/Common/Network/WebService.swift
@@ -8,46 +8,49 @@
 import Foundation
 
 enum WebService {
+  
+  enum Endpoint: String {
+    case base = "https://habitplus-api.tiagoaguiar.co"
     
-    enum Endpoint: String {
-        case base = "https://habitplus-api.tiagoaguiar.co"
-        
-        case postUser = "/users"
-    }
+    case postUser = "/users"
+  }
+  
+  private static func completeUrl(path: Endpoint) -> URLRequest? {
+    guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
+    
+    return URLRequest(url: url)
+  }
+  
+  static func postUser(request: SignUpRequest) {
+    guard let jsonData = try? JSONEncoder().encode(request) else { return }
+     
+    guard var urlRequest = completeUrl(path: .postUser) else { return }
+    
+    urlRequest.httpMethod = "POST"
+    urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
+    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    urlRequest.httpBody = jsonData
+    
+    let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+      guard let data = data, error == nil else {
+        print(error)
+        return
+      }
       
-    private static func completeUrl(path: Endpoint) -> URLRequest? {
-        guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil}
-                
-        return URLRequest(url: url)
+      print(String(data: data, encoding: .utf8))
+      
+      print("response\n")
+      
+      print(response)
+      
+      if let r = response as? HTTPURLResponse {
+        print(r.statusCode)
+      }
+      
     }
-        
-    static func postUser(request: SignUpRequest) {
-            
-            guard let jsonData = try? JSONEncoder().encode(request) else {return}
-            
-            guard var urlRequest = completeUrl(path: .postUser) else {return}
-            
-            urlRequest.httpMethod = "Post"
-            urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
-            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            urlRequest.httpBody = jsonData
-            
-            let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-                guard let data = data, error == nil else {
-                    print (error)
-                    return
-                }
-                print(String(data: data, encoding: .utf8))
-                
-                print("response\n")
-                print(response)
-                
-                if let r = response as? HTTPURLResponse {
-                    print(r.statusCode)
-                }
-            }
-            
-            task.resume()
-
-    }
+    
+    task.resume()
+    
+  }
+  
 }
