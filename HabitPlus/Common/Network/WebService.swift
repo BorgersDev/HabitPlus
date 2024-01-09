c2227ed3ffa2d4b8665d61702d7dbe66ffd89cd0
BorgesDev <borgesarthur.dev@gmail.com>
2024-01-08 22:24:30 -0300

    Making URL Session code Generic

--- HabitPlus/Common/Network/WebService.swift
+++ HabitPlus/Common/Network/WebService.swift
@@ -9,48 +9,89 @@ import Foundation
 
 enum WebService {
   
+  
   enum Endpoint: String {
     case base = "https://habitplus-api.tiagoaguiar.co"
     
     case postUser = "/users"
   }
+    enum NetworkError{
+        case badRequest
+        case notFound
+        case unauthorized
+        case internalServerError
+    }
+    
+    enum Result {
+        case sucess (Data)
+        case failure (NetworkError, Data?)
+    }
   
   private static func completeUrl(path: Endpoint) -> URLRequest? {
     guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
     
     return URLRequest(url: url)
   }
+    
+    private static func call<T: Encodable>(path: Endpoint,
+                                           body: T,
+                                           completion: @escaping (Result)-> Void){
+        guard let jsonData = try? JSONEncoder().encode(body) else { return }
+
+        guard var urlRequest = completeUrl(path: path) else { return }
+        
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+          guard let data = data, error == nil else {
+            print(error)
+              completion(.failure(.internalServerError, nil))
+            return
+          }
+            if let r = response as? HTTPURLResponse {
+                switch r.statusCode {
+                case 400:
+                    completion(.failure(.badRequest, data))
+                    break
+                case 404:
+                    completion(.failure(.notFound, data))
+                    break
+                case 200: completion(.sucess(data))
+                default:
+                    break
+                }
+            }
+          
+          
+          print("response\n")
+          
+          print(response)
+          
+          
+        }
+        
+        task.resume()
+
+    }
   
   static func postUser(request: SignUpRequest) {
-    guard let jsonData = try? JSONEncoder().encode(request) else { return }
-     
-    guard var urlRequest = completeUrl(path: .postUser) else { return }
-    
-    urlRequest.httpMethod = "POST"
-    urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
-    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    urlRequest.httpBody = jsonData
-    
-    let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-      guard let data = data, error == nil else {
-        print(error)
-        return
+      call(path: .postUser, body: request) { result in
+          switch result {
+          case .failure(let error, let data):
+              if let data = data {
+                  print(String(data: data, encoding: .utf8))
+
+              }
+              break
+          case .sucess(let data):
+              print(String(data: data, encoding: .utf8))
+
+          }
       }
-      
-      print(String(data: data, encoding: .utf8))
-      
-      print("response\n")
-      
-      print(response)
-      
-      if let r = response as? HTTPURLResponse {
-        print(r.statusCode)
-      }
-      
-    }
-    
-    task.resume()
-    
+        
   }
   
 }
